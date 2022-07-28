4fdb5f50d63bd56a707a06e650ec72b9a75544ac
Christopher Greaves <cgreaves@broadinstitute.org>
2022-07-28 15:46:40 -0400

    refactor prediction code to remove redundancy

--- main/predict_2.py
+++ main/predict_2.py
@@ -25,6 +25,10 @@ def unique_name():
 
 def predict_from_smiles(model_name, smiles_list):
 
+    """Takes a SMILES list and returns a DataFrame with the SMILES, their properties, and the prediction value.
+     Predicts based on the model name which must match a pkl file in the models directory, this will always be true
+     from the web interface."""
+
     with open("main/models/" + model_name + ".pkl", "rb") as f:
         model = pickle.load(f)
 
@@ -37,38 +41,34 @@ def predict_from_smiles(model_name, smiles_list):
     vals = df[PROPERTY_NAMES].astype(float)
 
     result_list = model.predict(vals)
+    df["prediction"] = result_list
 
-    prediction = zip(list(df["SMILES"]), result_list)
-    # we are getting the SMILES back out of the df because we may have discarded some invalid ones
-
-    df2 = pd.DataFrame()
-    df2["SMILES"] = df["SMILES"]
-    df2[model_name] = result_list
-    fname = model_name + "_" + unique_name() + ".csv"
-    df2.to_csv("main/static/output/" + fname, index=False)
-
-    return prediction, fname  # provide the name where file is saved so the user can download it
+    return df  # various functions that call this function can do what they want with the dataframe
 
 
 def predict_single(model_name, smiles):
 
-    """For the URL API. Some code duplication for now."""
+    """For the URL API."""
 
-    #TODO: error messages like model name not recognised, etc
-    with open("main/models/" + model_name + ".pkl", "rb") as f:
-        model = pickle.load(f)
-
-    df = pd.DataFrame({"SMILES": [smiles]})  # model expects a DataFrame
-    df["props"] = df["SMILES"].apply(smi2props, 1)
-    df.dropna(inplace=True)  # throw out rows where we got None for property calculation because the SMILES was bad
-    df[PROPERTY_NAMES] = df['props'].to_list()  # PROPERTY_NAMES is a list of column headings and we are expanding
-    # the list that is in the "props" cell
-
-    vals = df[PROPERTY_NAMES].astype(float)
-
-    result_list = model.predict(vals)
-
-    prediction = result_list[0]  # just one result
-    # we are getting the SMILES back out of the df because we may have discarded some invalid ones
+    pred = predict_from_smiles(model_name, [smiles])  # pass our single SMILES as a list of length 1
+    prediction = pred["prediction"][0]  # there is just one result in a dataframe with one row
 
     return prediction
+
+
+def predict_multiple(model_name, smiles_list, write_file=True):
+
+    df = predict_from_smiles(model_name, smiles_list)
+    prediction = zip(list(df["SMILES"]), df["prediction"])
+    # we are getting the SMILES back out of the df because we may have discarded some invalid ones
+
+    if write_file:  # set up a new df with minimal columns to write to a csv
+        df2 = pd.DataFrame()
+        df2["SMILES"] = df["SMILES"]
+        df2[model_name] = df["prediction"]
+        fname = model_name + "_" + unique_name() + ".csv"
+        df2.to_csv("main/static/output/" + fname, index=False)  # put here so they can be served as static files
+    else:
+        fname = None  # may as well be consistent and always return two things
+
+    return prediction, fname  # provide the name where file is saved so the user can download it
--- main/start.py
+++ main/start.py
@@ -9,11 +9,15 @@ bp = Blueprint('lookup_page', __name__, url_prefix='')
 
 def get_models():
 
+    """Gets a list of the names of all pickled model files in the model directory, to present in dropddown list"""
+
     return [os.path.splitext(x)[0] for x in os.listdir(r"./main/models")]
 
 
 @bp.route('/', methods=('GET', 'POST'))
 def begin():
+
+    """Main page method for interactive prediction via textentry box"""
 
     error = None
 
@@ -21,17 +25,16 @@ def begin():
 
         query = request.form['qry']
         mod = request.form['model']
-        print(f"Model is {mod}")
+        print(f"Predicting values using {mod} model")
         smiles_list = query.rstrip("\r\n").split(os.linesep)
         smiles_list = [x.strip("\r\n") for x in smiles_list]  # get rid of the damn extra newlines!!
-        result, file_location = predict_2.predict_from_smiles(mod, smiles_list)
+        result, file_location = predict_2.predict_multiple(mod, smiles_list)
         file_link = "/result/" + file_location
         return render_template('page1/page1.html', results=result, model_list=get_models(), file_loc=file_link)
 
     if request.method == 'GET':
 
-        get_models()
-        pass
+        pass  # nothing extra to do beyond the standard render template
 
     if error:
         flash(error)
@@ -56,15 +59,16 @@ def url_prediction(model, smiles):
 @bp.route('/predict/<model>', methods=('POST',))
 def post_prediction(model):
 
-    """Expects a POST of comma-delimited SMILES strings, like PubChem"""
+    """Expects a POST of comma-delimited SMILES strings, like PubChem. Returns a JSON dict of smiles: value
+    inside another dict that gives the name of the prediction model that was passed in the URL.
+    e.g {"solubility":{"CCN":1.0799283637300687,"CNC":1.1766386397282302}}"""
 
     try:
         smiles_list = request.data.decode("UTF-8").split(",")  # data arrives in binary format, convert to list of strs
-        print(smiles_list)
     except:
         return jsonify({"Fault": "The text of the request could not be decoded."})
 
-    result, _ = predict_2.predict_from_smiles(model, smiles_list)  # don't care about the file name, not using it
+    result, _ = predict_2.predict_multiple(model, smiles_list, write_file=False)
 
     outdc = {}  # where we will store the predictions from the results zip object
     outer = {model:outdc}
